354512e232d2630e6b86ca7593ee463a3e3e5d49
rrc <raul.elfriky@gmail.com>
2015-10-16 22:58:59 +0200

    [IMP] minimal functionality with classes

--- combat.py
+++ combat.py
@@ -1,4 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-print 'hello world'
+print '== RPG COMBAT MODULE =='
+
+NAME = 0
+INITIATIVE = 1
+HP = 2
+
+class Player:
+    def __init__(self, name='_player_', initiative=0, hp=0):
+        self.name = name
+        self.initiative = initiative
+        self.hp = hp
+
+    def show_player(self):
+        return (self.name, self.initiative, self.hp)
+
+    def hit_player(self, amount=0):
+        self.hp += amount
+
+    def change_initiative(self, initiative):
+        self.initiative = initiative
+
+class Combat:
+    def __init__(self, players=[], rounds=0):
+        self.players=players
+        self.rounds=rounds
+
+    def add_player(self, player):
+        self.players.append(player)
+
+    def add_players(self, players):
+        self.players += players
+
+    def next_round(self):
+        self.rounds += 1
+
+
+
+juanma = Player('Juanma',13,1)
+print juanma.show_player()
+juanma.hit_player(3)
+print juanma.show_player()
